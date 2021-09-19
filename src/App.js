3edef6552f6e5ebcf49f6ccacb589a4e226d8677
kumarrohan2804 <kumarrohan2804@gmail.com>
2021-09-20 01:22:49 +0530

    income and expense card skeleton created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import "./App.css";
-
+import React from "react";
+import { Grid } from "@material-ui/core";
+import Details from "./components/details/Details";
+import useStyles from "./styles";
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <h1>React App</h1>
+    <div>
+      <Grid
+        className={classes.grid}
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4} className={classes.mobile}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={3} className={classes.main}>
+          Main
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.desktop}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.last}>
+          <Details title="Expense" />
+        </Grid>
+        {/* <PushToTalkButtonContainer>
+          <PushToTalkButton />
+        </PushToTalkButtonContainer> */}
+      </Grid>
     </div>
   );
 }
